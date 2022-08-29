f379891ca96bfb9dcb36cb30f4dd0dd057441c4a
Juan Jose Talenti <jjtalenti41@gmail.com>
2022-08-29 00:48:52 -0300

    Angular con Header, Acerca De, y Experiencia con JSON

--- src/app/components/experiencia/experiencia.component.ts
+++ src/app/components/experiencia/experiencia.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PorfolioService } from 'src/app/services/porfolio.service';
 
 @Component({
   selector: 'app-experiencia',
@@ -6,10 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./experiencia.component.css']
 })
 export class ExperienciaComponent implements OnInit {
+  experiencia:any;
 
-  constructor() { }
+  constructor(private datosPorfolio: PorfolioService) { }
 
   ngOnInit(): void {
+    this.datosPorfolio.obtenerDatos().subscribe(data => {
+      this.experiencia = data;
+    })
   }
-
 }
