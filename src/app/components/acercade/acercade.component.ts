c3157adbfc6634d425b93fcd3c9c5fe5dda2c12a
Juan Jose Talenti <jjtalenti41@gmail.com>
2022-08-28 23:46:25 -0300

    Angular con JSON en Header y Acerca De

--- src/app/components/acercade/acercade.component.ts
+++ src/app/components/acercade/acercade.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { PorfolioService } from 'src/app/services/porfolio.service';
 
 @Component({
   selector: 'app-acercade',
   templateUrl: './acercade.component.html',
   styleUrls: ['./acercade.component.css']
 })
-export class AcercadeComponent implements OnInit {
 
-  constructor() { }
+export class AcercadeComponent implements OnInit {
+  acercaDe:any;
+
+  constructor(private datosPorfolio: PorfolioService) { }
 
   ngOnInit(): void {
+    this.datosPorfolio.obtenerDatos().subscribe(data => {
+        this.acercaDe = data;
+    })
   }
-
 }
