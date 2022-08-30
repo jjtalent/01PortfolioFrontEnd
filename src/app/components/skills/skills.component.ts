61eb011519fb042547caac73be54ccce4db891fc
Juan Jose Talenti <jjtalenti41@gmail.com>
2022-08-29 23:03:28 -0300

    Angular con Header, Acerca De, Experiencia, Educación, Skills e Idiomas en JSON.

--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PorfolioService } from 'src/app/services/porfolio.service';
 
 @Component({
   selector: 'app-skills',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./skills.component.css']
 })
 export class SkillsComponent implements OnInit {
+  skills:any;
 
-  constructor() { }
+  constructor(private datosPorfolio: PorfolioService) { }
 
   ngOnInit(): void {
+    this.datosPorfolio.obtenerDatos().subscribe(data => {
+      this.skills = data;
+    });
   }
 
 }
