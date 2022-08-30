4a4b5b4adbbb2ff79af2ebe16f5f91f1d9102083
Juan Jose Talenti <jjtalenti41@gmail.com>
2022-08-29 22:31:35 -0300

    Angular con Header, Acerca De, Experiencia y Educación en JSON.

--- src/app/components/educacion/educacion.component.ts
+++ src/app/components/educacion/educacion.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PorfolioService } from 'src/app/services/porfolio.service';
 
 @Component({
   selector: 'app-educacion',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./educacion.component.css']
 })
 export class EducacionComponent implements OnInit {
-
-  constructor() { }
+  educacion:any;
+  
+  constructor(private datosPorfolio: PorfolioService) { }
 
   ngOnInit(): void {
+    this.datosPorfolio.obtenerDatos().subscribe(data => {
+      this.educacion = data;
+    })
   }
 
 }
